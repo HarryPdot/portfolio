0cd2b0d25611aeccb4cf9c067fde2d7d5c8124de
HarryP <harrypham__@hotmail.com>
2022-02-24 19:02:09 +1100

    add images to projectbox

--- src/ProjectBox.js
+++ src/ProjectBox.js
@@ -1,18 +1,21 @@
 import './ProjectBox.css'
+import portraitImage from './images/600x600.jpg'
 
 export default function ProjectBox({ project, setProjects, projects }) {
-
     const handleDetail = () => {
         let projectArr = projects
         let index = projectArr.indexOf(project)
         projectArr[index].arr.status = !projectArr[index].arr.status
         setProjects([...projectArr])
     }
-
+    console.log(project)
     var skillArr = project.arr.skills.split(',')
     return (
         <div className='project-box'>
             <div className='project-detail-box animate__animated animate__fadeIn'>
+                <div className='project-image-container'>
+                    <img className="project-images" src={project.arr.image.filename} alt="" />
+                </div>
                 <div className='project-detail-grid'>
                     <div className='project-detail-heading'><span className='project-detail-num'>01. </span>{project.arr.name}</div>
                     <div className='project-detail-skill-grid'>
--- src/ProjectBoxDetail.js
+++ src/ProjectBoxDetail.js
@@ -16,7 +16,7 @@ export default function ProjectBoxDetail({ project, setProjects, projects }) {
             </div>
             <div className='project-box-details-grid animate__animated animate__fadeIn'>
                 <div className='project-box-detail-title'>02. Project Title</div>
-                <div className='project-box-detail-description'> My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.</div>
+                <div className='project-box-detail-description'> My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.</div>
                 <div className='project-box-detail-skills-grid'>
                     <div className='project-box-detail-skills'>yes</div>
                 </div>
