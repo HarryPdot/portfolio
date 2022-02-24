6dfb301f9191e215b2c0507ce2216815eb35724f
HarryP <harrypham__@hotmail.com>
2022-02-25 00:41:33 +1100

    update image opacity

--- src/ProjectBox.js
+++ src/ProjectBox.js
@@ -12,18 +12,22 @@ export default function ProjectBox({ project, setProjects, projects }) {
     var skillArr = project.arr.skills.split(',')
     return (
         <div className='project-box'>
-            <div className='project-detail-box animate__animated animate__fadeIn'>
+            <div className='image-grid'>
                 <div className='project-image-container'>
                     <img className="project-images" src={project.arr.image.filename} alt="" />
+                    <div></div>
                 </div>
-                <div className='project-detail-grid'>
-                    <div className='project-detail-heading'><span className='project-detail-num'>01. </span>{project.arr.name}</div>
-                    <div className='project-detail-skill-grid'>
-                        {skillArr.map(skill => {
-                            return <div className="project-detail-skills">{skill}</div>
-                        })}
+                <div className='project-detail-box animate__animated animate__fadeIn'>
+
+                    <div className='project-detail-grid'>
+                        <div className='project-detail-heading'><span className='project-detail-num'>01. </span>{project.arr.name}</div>
+                        <div className='project-detail-skill-grid'>
+                            {skillArr.map(skill => {
+                                return <div className="project-detail-skills">{skill}</div>
+                            })}
+                        </div>
+                        <div className="project-details">{project.arr.description}</div>
                     </div>
-                    <div className="project-details">{project.arr.description}</div>
                 </div>
             </div>
             <div className='project-view' onClick={handleDetail}> View details </div>
