86d95247a9ae352898ca06c4628321b679ac6ffe
HarryP <harrypham__@hotmail.com>
2022-02-25 14:53:57 +1100

    add detailed description to projectbox

--- src/ProjectBoxDetail.js
+++ src/ProjectBoxDetail.js
@@ -8,7 +8,7 @@ export default function ProjectBoxDetail({ project, setProjects, projects }) {
         projectArr[index].arr.status = !projectArr[index].arr.status
         setProjects([...projectArr])
     }
-
+    var skillArr = project.arr.skills.split(',')
     return (
         <div className='project-box-detail-container'>
             <div className="close-btn-container">
@@ -16,9 +16,13 @@ export default function ProjectBoxDetail({ project, setProjects, projects }) {
             </div>
             <div className='project-box-details-grid animate__animated animate__fadeIn'>
                 <div className='project-box-detail-title'>02. <span>Project Title</span></div>
-                <div className='project-box-detail-description'> My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.My best days are when I am working with a computer in front of me. I have a love of learning and learn new programs very quickly.</div>
+                <div className='project-box-detail-description'>{project.arr['detailed_description']}</div>
                 <div className='project-box-detail-skills-grid'>
-                    <div className='project-box-detail-skills'>yes</div>
+                    
+                    {skillArr.map(skill => {
+                        return <div className="project-box-detail-skills">{skill}</div>
+                    })}
+
                 </div>
             </div>
             <a href={project.arr.url} target="_blank" className='project-box-detail-demo'>View Demo</a>
