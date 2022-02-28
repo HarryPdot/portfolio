4d922d4cd1b9388b67c3f04b09f9fe5627136490
HarryP <harrypham__@hotmail.com>
2022-02-28 19:52:43 +1100

    update aboutme

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -16,6 +16,7 @@ export default function AboutMe() {
                         <div className='aboutme-title'>About Me</div>
                         <div className='aboutme-description'> I am a software developer, and I previously studied business. I have a passion for learning, and with how broad program languages are, I find it very interesting how everything I learnt can connect with one another. I am open for opportunities that will expand my knowledge, and would love to work with a team with similar mindsets.  </div>
                         <p className="aboutme-description"> My hobbies are playing strategic games and spending time with my dogs. I try to stay proactive with a healthy lifestyle going to the gym, and trying to eat healthy.</p>
+                        <p className="aboutme-description">my list of skills... </p>
                         <a href={resume} target="_blank" className='resume-button' >View my resume</a>
                     </div>
                 </div>
