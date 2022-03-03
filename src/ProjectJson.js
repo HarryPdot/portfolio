0126616214228fb66a900dc9564ee77749c9e873
HarryP <harrypham__@hotmail.com>
2022-03-03 19:08:32 +1100

    update project details

--- src/ProjectJson.js
+++ src/ProjectJson.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 
 export default function Project() {
-    return axios.get('https://api.storyblok.com/v2/cdn/stories/projects?cv=1645760853&token=QYvQtCHehfgMxuXzqVeecwtt&version=published')
+    return axios.get('https://api.storyblok.com/v2/cdn/stories/projects?cv=1646294829&token=QYvQtCHehfgMxuXzqVeecwtt&version=published')
         .then(res => {
             let newArr = []
             res.data.story.content.body.forEach(arr => {
